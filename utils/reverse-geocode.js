ce9265ce0cb5ff8bb218323c819caa44bac77771
Giovanni Pinto <giovanni.pinto.po@gmail.com>
2017-11-13 17:10:23 +0100

    Implemented geocoding request

--- utils/reverse-geocode.js
+++ utils/reverse-geocode.js
@@ -1,9 +1,31 @@
+const querystring = require('querystring')
+const Promise = require('bluebird')
+const eachLimit = require('async/eachLimit')
 const fetch = require('node-fetch')
-const gyms = require('../gyms.json')
+
+const api = 'https://nominatim.openstreetmap.org/reverse'
+
+async function addCity (g, callback) {
+  const params = {
+    lat: 43.313176,
+    lon: 11.328049,
+    format: 'json'
+  }
+  const res = await fetch(`${api}?${querystring.stringify(params)}`, {
+    headers: { 'User-Agent': 'pogo-gyms-bot' }
+  })
+  const body = await res.json()
+  g.city = body.address.city
+  callback()
+}
 
 async function Main() {
-  // https://nominatim.openstreetmap.org/reverse?lat=43.313176&lon=11.328049&format=json
-  
+  const gyms = require('../gyms.json').slice(0, 1)
+
+  eachLimit(gyms, 1, addCity, function(err) {
+    console.log(err, gyms)
+  })
+
 } 
 
 Main()
