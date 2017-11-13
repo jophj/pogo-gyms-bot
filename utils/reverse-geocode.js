86cce6c7900cbda1b847ae1a7c4e20e20980a1b1
Giovanni Pinto <giovanni.pinto.po@gmail.com>
2017-11-13 16:13:38 +0100

    Installed node-fetch

---
+++ utils/reverse-geocode.js
@@ -0,0 +1,11 @@
+const fetch = require('node-fetch')
+const gyms = require('../gyms.json')
+
+async function Main() {
+  // https://nominatim.openstreetmap.org/reverse?lat=43.313176&lon=11.328049&format=json
+  
+} 
+
+Main()
+  .then()
+  .catch()
