14f6b4afe7c63dce8ab1e78bd77c8b5b52b00e1f
Giovanni Pinto <giovanni.pinto.po@gmail.com>
2017-11-13 16:07:50 +0100

    Lunr is used as indexer

--- repositories/gyms.js
+++ repositories/gyms.js
@@ -1,13 +1,29 @@
-const db = require('../gyms.json')
+const lunr = require('lunr')
+require("lunr-languages/lunr.stemmer.support")(lunr)
+require("lunr-languages/lunr.it")(lunr)
+
+const gyms = require('../gyms.json')
+const db = {}
+
+console.log('Initializing index for', gyms.length, 'gyms')
+var idx = lunr(function () {
+  this.use(lunr.it)
+  this.field('name')
+  this.field('city')
+
+  gyms.forEach(function (g) {
+    db[g.id] = g
+    this.add(g)
+  }, this)
+})
+console.log('...done.')
 
 function searchGym(queryString) {
   if (!queryString) return []
-  const results = db.filter(g => {
-    let tokens = queryString.trim().split(' ')
-    return tokens.every(t => {
-      return g.name.toLowerCase().indexOf(t.toLowerCase()) > -1
-    })
-  })
+
+  const results = idx
+    .search(queryString)
+    .map(r => db[r.ref])
 
   return results
 }
